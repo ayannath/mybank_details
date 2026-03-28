d7f577cef8808bce547c7cede65a590353aaa5d7
Ayan Nath <mymail_ayaan@yahoo.co.in>
2026-03-28 17:06:46 +0000

    fragment created dialog

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -5,6 +5,24 @@ sap.ui.define([
 
     return Controller.extend("com.sap.mybankdetails.controller.App", {
         onInit() {
+        },
+
+        // open popup "MoreDetails.fragment.xml"
+        onOpenBankDetails: function () {
+            // create logic for dialog
+            if (!this.mbd_moreDetails) {
+                this.mbd_moreDetails = this.loadFragment(
+                    { name: "com.sap.mybankdetails.view.fragments.MoreDetails" }
+                );
+            }
+            this.mbd_moreDetails.then(function (oDialog) {
+                oDialog.open();
+            });
+        },
+        // close dialog "MoreDetails.fragment.xml"
+        onMoreDetailsClose: function () {
+            this.byId("mbd_moreDetails").close();
         }
+
     });
 });
