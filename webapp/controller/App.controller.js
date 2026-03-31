a4892ebabf9993a73106c42fecdd42ff39537aac
Ayan Nath <mymail_ayaan@yahoo.co.in>
2026-03-31 08:00:49 +0000

    i18n spanish translation

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -5,6 +5,38 @@ sap.ui.define([
 
     return Controller.extend("com.sap.mybankdetails.controller.App", {
         onInit() {
+            // alert("this is onInit block");
+            debugger
+
+            /* ============== checking browser language and setting the global resource model
+             *  change the language from setting in browser to take effect 
+                //in "console"
+                var i18nModel = this.getOwnerComponent().getModel("i18n_es")
+                this.getOwnerComponent().setModel(i18nModel, "i18n")
+            */
+            /*if (navigator.language == "en") {
+                var i18nModel = this.getOwnerComponent().getModel("i18n")
+                this.getOwnerComponent().setModel(i18nModel, "i18n")
+            } 
+            else if (navigator.language == "es") {
+                var i18nModel = this.getOwnerComponent().getModel("i18n_es")
+                this.getOwnerComponent().setModel(i18nModel, "i18n")
+            }
+            else {
+                var i18nModel = this.getOwnerComponent().getModel("i18n")
+                this.getOwnerComponent().setModel(i18nModel, "i18n")
+            }*/
+
+            let appLang;
+            if (navigator.language == "en")
+                appLang = "i18n";
+            else if (navigator.language == "es")
+                appLang = "i18n_es";
+            else
+                appLang = "i18n";
+            var i18nModel = this.getOwnerComponent().getModel(appLang)
+            this.getOwnerComponent().setModel(i18nModel, "i18n")
+
         },
 
         // open popup "MoreDetails.fragment.xml"
