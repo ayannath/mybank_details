8d89dec31c07e95cf11905d91af43e4ff975f14c
Ayan Nath <mymail_ayaan@yahoo.co.in>
2026-04-06 09:27:05 +0000

    odata v1

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -5,8 +5,67 @@ sap.ui.define([
 
     return Controller.extend("com.sap.mybankdetails.controller.App", {
         onInit() {
+
+            let oData = {
+                // Property Binding
+                bankDetails: {
+                    "AccNum": "38473***********",   // {/bankDetails/AccNum} // relative path
+                    "Name": "Ayan Nath",            // {/bankDetails/Name}
+                    "Ifsc": "000000*****",          // {/bankDetails/ifscode}
+                },
+                ifscode: "RBI032145", // absolute path {/ifscode}
+                // Element/Context Binding "MoreDetails.fragment.xml"
+                accountDetails: {
+                    "AccNum": "38473***********",   // {/bankDetails/AccNum} // relative path
+                    "Name": "Ayan Nath",            // {/bankDetails/Name}
+                    "Ifsc": "000000*****",          // {/bankDetails/ifscode}
+                    "CustID": "*****12345",
+                    "Address": {
+                        "city": "New York",
+                        "zipcode": "123456",
+                        "house": "001",
+                        "street": "Wall Street",
+                        "state": "California",
+                        "country": "USA"
+                    }
+                },
+                // Aggregation/List Binding
+                cardDetails: [
+                    {
+                        "cardType": "debit card",
+                        "cardNum": "0000-0012-3451-1111",
+                        "cardCompany": "master card",
+                        "assinDate": "23 Aug 2017",
+                        "state": true
+                    },
+                    {
+                        "cardType": "credit card",
+                        "cardNum": "0000-0012-3451-2222",
+                        "cardCompany": "visa card",
+                        "assinDate": "17 Aug 2020",
+                        "state": false
+                    },
+                    {
+                        "cardType": "debit card",
+                        "cardNum": "0000-0012-3451-3333",
+                        "cardCompany": "rupay card",
+                        "assinDate": "20 Aug 2016",
+                        "state": true
+                    }
+                ]
+            };
+
+            // common for all
+            let oModel = new sap.ui.model.json.JSONModel();     // declaration of model
+            oModel.setData(oData);                              // store data in model
+            this.getView().setModel(oModel);                    // data applied to view label
+
+            // new Model from same oData object for list
+            let lModel = new sap.ui.model.json.JSONModel(oData);
+            this.getView().setModel(lModel, "oBankDetails");    // "oBankDetails" is "JSON model" name
+
             // alert("this is onInit block");
-            debugger
+            // debugger
 
             /* ============== checking browser language and setting the global resource model
              *  change the language from setting in browser to take effect 
